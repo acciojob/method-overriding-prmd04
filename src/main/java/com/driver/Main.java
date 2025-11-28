d732fb38f888de8138decaa82ea5c3525eae258f
pramod ade <prmdextracthere@gmail.com>
2025-11-28 17:13:56 +0530

    test 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
-public class Main {
-  
+public class A {
+  public String method1(){
+    return "method is overiden in class B";
+  }
+}
+public class B extends A{
+  public String method1(){
+    return "Method is overridden in Extended class B";
+  }
+}
+public class Main{
+  public static void main(String args[]){
+    B obj = new B();
+    System.out.println(obj.method1());
+
+      A objA = new B();
+      System.out.println(objA.method1());
+  }
 }
