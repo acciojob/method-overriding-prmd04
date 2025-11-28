b66e09549833c2ac4cf1095dff1429b1db1824cd
pramod ade <prmdextracthere@gmail.com>
2025-11-28 17:26:31 +0530

    test 4

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,8 +1,20 @@
 package com.driver.test;
 
-import static org.junit.Assert.*;
-import org.junit.jupiter.api.Test;
+import com.driver.A; 
+import com.driver.B;  
 
 public class TestCases {
-    
+
+    @Test
+    public void testInvokeAFromB() {
+        A obj = new B();
+        assertEquals("Method is overridden in Extended class B", obj.meth());
+    }
+
+    @Test
+    public void testOverrideInB() {
+        B objB = new B();
+        assertEquals("Method is overridden in Extended class B", objB.meth());
+    }
+
 }
